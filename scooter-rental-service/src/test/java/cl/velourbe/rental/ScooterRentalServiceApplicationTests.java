311b88b0adea5416b2b267e2ef38175a9df4b30e
Ethan Astorga <ethanastorga311@gmail.com>
2026-06-22 19:08:39 -0400

    test: pruebas de controller y perfil de test con base de datos h2

--- scooter-rental-service/src/test/java/cl/velourbe/rental/ScooterRentalServiceApplicationTests.java
+++ scooter-rental-service/src/test/java/cl/velourbe/rental/ScooterRentalServiceApplicationTests.java
@@ -2,8 +2,10 @@ package cl.velourbe.rental;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ScooterRentalServiceApplicationTests {
     @Test
     void contextLoads() {}
--- user-auth-service/src/test/java/cl/velourbe/userauth/UserAuthServiceApplicationTests.java
+++ user-auth-service/src/test/java/cl/velourbe/userauth/UserAuthServiceApplicationTests.java
@@ -2,8 +2,10 @@ package cl.velourbe.userauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserAuthServiceApplicationTests {
     @Test
     void contextLoads() {}
